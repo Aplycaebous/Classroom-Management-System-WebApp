2d0cfbed1196507f289194b1c6bd6a0d485aea12
aplycaebous <farhanishmam@gmail.com>
2020-12-25 14:12:06 +0600

    commit

--- bookRoomCR.js
+++ bookRoomCR.js
@@ -7,7 +7,7 @@ function loadBuildingList()
 {
     db.collection('Building').get().then(snapshot =>
     {
-        var html = ``;
+        var html = `<option value = "">Any</option>`;
         const data = snapshot.docs;
         data.forEach(doc => 
         {
@@ -18,5 +18,45 @@ function loadBuildingList()
     });
 }
 
+//Search Rooms based on credentials
+
+function searchRoom(building, AC, projector, board, capacity)
+{
+    db.collection("Room").where("AC","==", AC).where("Projector","==",projector).where("Board","==",Number(board)).where("Capacity",">=",Number(capacity)).get().then
+    ( 
+        snapshot =>
+        {
+            var roomIDList = [];
+            const snapData = snapshot.docs;
+            snapData.forEach(doc => 
+            {
+                if((building == "") || (building === doc.id.charAt(0)))
+                { 
+                    console.log(doc.id);
+                   roomIDList.push(doc.id);
+                }
+            })
+            //checks record
+            //dispays roomlist                 
+        }
+    )
+   
+}
+
+
+
 //Make Room List based on Room credentials 
+
+const submitForm = document.querySelector("#submit-form");
+submitForm.addEventListener('submit', (e) => 
+{
+    e.preventDefault();
+
+    buildingNo = submitForm['BuildingNo'].value;
+    capacity = submitForm['capacity'].value;
+    AC = submitForm['AC'].checked;
+    projector = submitForm['Projector'].checked;
+    board = submitForm['Boards'].value;
+    searchRoom(buildingNo, AC, projector, board, capacity);   
+})
 
